a85a12c53f50de8a8dc13b6c652222c3cbec2f57
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-16 22:42:00 +0500

    Updated

--- While/while.py
+++ While/while.py
@@ -13,19 +13,36 @@
 # height = float(height)
 
 # WHILE
-num = 1
-while num <= 5:
-    print(num)
-    num += 1
+# num = 1
+# while num <= 5:
+#     print(num)
+#     num += 1
 
-print("Kiritilgan sonning kvadratini chiqaruvchi dastur.")
-question = "Istalgan son kiriting "
-question += "(dasturni to'xtatish uchun 'exit' deb yozing): "
-value = ''
-while value != 'exit':
-    value = input(question)
-    if value != 'exit':
-        print(float(value)**2)
+# print("Kiritilgan sonning kvadratini chiqaruvchi dastur.")
+# question = "Istalgan son kiriting "
+# question += "(dasturni to'xtatish uchun 'exit' deb yozing): "
+# value = ''
+# while value != 'exit':
+#     value = input(question)
+#     if value != 'exit':
+#         print(float(value)**2)
+
+# print("Kiritilgan sonning kvadratini chiqaruvchi dastur.")
+# question = "Istalgan son kiriting "
+# question += "(dasturni to'xtatish uchun 'exit' deb yozing): "
+# sign = True
+# while sign:
+#     value = input(question)
+#     if value == 'exit':
+#         sign = False
+#     else:
+#         print(float(value)**2)
+
+
+
+
+
+
 
 
 
