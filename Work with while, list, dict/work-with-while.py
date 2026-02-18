742d74b848e1a5af0611558f2459d9ffebf6c366
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-18 15:34:38 +0500

    remove in the while loop

--- Work with while, list, dict/work-with-while.py
+++ Work with while, list, dict/work-with-while.py
@@ -13,17 +13,23 @@
 # for friend in friends:
 #     print(friend.title())
 
-friends = {}
-sign = True
-while sign:
-    name = input("Do'stingizni ismini kiriting: ")
-    age = input(f"{name.title()}ning yoshini kiriting: ")
-    friends[name] = int(age)
+# friends = {}
+# sign = True
+# while sign:
+#     name = input("Do'stingizni ismini kiriting: ")
+#     age = input(f"{name.title()}ning yoshini kiriting: ")
+#     friends[name] = int(age)
+#
+#     result = input("Yana ma'lumot qo'shasizmi? (ha/yo'q): ")
+#     if result != 'ha':
+#         break
+#
+# for name, age in friends.items():
+#     print(f"{name.title()} {age} yoshda.")
 
-    result = input("Yana ma'lumot qo'shasizmi? (ha/yo'q): ")
-    if result != 'ha':
-        break
+cars = ['nexia', 'toyota', 'bmw', 'nexia', 'malibu', 'kia k5', 'nexia', 'rolls roys']
+car = 'nexia'
+while car in cars:
+    cars.remove(car)
 
-for name, age in friends.items():
-    print(f"{name.title()} {age} yoshda.")
-
+print(cars)
