8996e73060aa192aaeb7a1b89379037f0664f706
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-18 15:28:23 +0500

    Do'stlar ma'lumotlarini lug'atga saqlash

--- Work with while, list, dict/work-with-while.py
+++ Work with while, list, dict/work-with-while.py
@@ -1,15 +1,29 @@
-print("Yaqin do'stlaringiz ro'yxatini tuzamiz.")
-friends = []
-n = 1
-while True:
-    question = f"{n}-do'stingizni ismini kiriting: "
-    name = input(question)
-    friends.append(name)
-    repeat = input("Yana ism qo'shishni hohlaysizmi (ha/yo'q): ")
-    n += 1
-    if repeat != 'ha':
+# print("Yaqin do'stlaringiz ro'yxatini tuzamiz.")
+# friends = []
+# n = 1
+# while True:
+#     question = f"{n}-do'stingizni ismini kiriting: "
+#     name = input(question)
+#     friends.append(name)
+#     repeat = input("Yana ism qo'shishni hohlaysizmi (ha/yo'q): ")
+#     n += 1
+#     if repeat != 'ha':
+#         break
+#
+# for friend in friends:
+#     print(friend.title())
+
+friends = {}
+sign = True
+while sign:
+    name = input("Do'stingizni ismini kiriting: ")
+    age = input(f"{name.title()}ning yoshini kiriting: ")
+    friends[name] = int(age)
+
+    result = input("Yana ma'lumot qo'shasizmi? (ha/yo'q): ")
+    if result != 'ha':
         break
 
-for friend in friends:
-    print(friend.title())
+for name, age in friends.items():
+    print(f"{name.title()} {age} yoshda.")
 
