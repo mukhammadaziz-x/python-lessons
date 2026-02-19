3dffba9278f432ab9d0373d3cd993a605245d24a
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-19 17:54:57 +0500

    My own range function as rename oraliq()

--- Return value from function/return-value-function.py
+++ Return value from function/return-value-function.py
@@ -30,24 +30,35 @@ from supabase_auth import model
 # student = toliq_ism_yasa('ikrom', 'zokirov', "hoshim ogli")
 # print(student)
 
-def avto_info(company, model, color, box, make_year, price=None):
-    avto = {
-        'company': company,
-        'model': model,
-        'color': color,
-        'box': box,
-        'make_year': make_year,
-        'price': price
-    }
-    return avto
+# def avto_info(company, model, color, box, make_year, price=None):
+#     avto = {
+#         'company': company,
+#         'model': model,
+#         'color': color,
+#         'box': box,
+#         'make_year': make_year,
+#         'price': price
+#     }
+#     return avto
+#
+# avto1 = avto_info('bmw', 'x7', 'qora', 'avtomat', 2026)
+# avto2 = avto_info('gm', 'gentra', 'oq', 'mexanika', 2023, '200,000,000')
+# avtolar = [avto1, avto2]
+# print("Onlayn bozorda mavjud bo'lgan mashinalar:")
+# for avto in avtolar:
+#     if avto['price']:
+#         price = avto['price']
+#     else:
+#         price = "Noma'lum"
+#     print(f"{avto['color'].title()} {avto['model'].title()}, Narhi: {price}")
 
-avto1 = avto_info('bmw', 'x7', 'qora', 'avtomat', 2026)
-avto2 = avto_info('gm', 'gentra', 'oq', 'mexanika', 2023, '200,000,000')
-avtolar = [avto1, avto2]
-print("Onlayn bozorda mavjud bo'lgan mashinalar:")
-for avto in avtolar:
-    if avto['price']:
-        price = avto['price']
-    else:
-        price = "Noma'lum"
-    print(f"{avto['color'].title()} {avto['model'].title()}, Narhi: {price}")
+# My own range function as rename oraliq()
+def oraliq(min, max):
+    numbers = []
+    while min < max:
+        numbers.append(min)
+        min += 1
+    return numbers
+
+for num in oraliq(1, 31):
+    print(num)
