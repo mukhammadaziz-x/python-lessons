bbd7c92ace170ad45f525b38101a7e834744f9e5
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-19 17:46:17 +0500

    Online car store function

--- Return value from function/return-value-function.py
+++ Return value from function/return-value-function.py
@@ -4,6 +4,8 @@
 #     print(f"{full_name.title()}")
 #
 # toliq_ism_yasa('muhammadaziz', 'xabibullayev')
+from supabase_auth import model
+
 
 # def toliq_ism_yasa(name, surname):
 #     """To'liq ism qaytaruvchi funksiya"""
@@ -18,12 +20,34 @@
 # print(f"Bugun optolmolog {doctor.title()} ishga kelmadi. U bugun dam oladi.")
 # print(f"Programming'dan kiradigan ustozinglar {teacher.title()} bugun betob ekan. Bugun 1 para dars bo'lmaydi.")
 
-def toliq_ism_yasa(name, surname, middlename=''):
-    if middlename:
-        full_name = f"{name} {surname} {middlename}"
-    else:
-        full_name = f"{name} {surname}"
-    return full_name.title()
+# def toliq_ism_yasa(name, surname, middlename=''):
+#     if middlename:
+#         full_name = f"{name} {surname} {middlename}"
+#     else:
+#         full_name = f"{name} {surname}"
+#     return full_name.title()
+#
+# student = toliq_ism_yasa('ikrom', 'zokirov', "hoshim ogli")
+# print(student)
 
-student = toliq_ism_yasa('ikrom', 'zokirov', "hoshim ogli")
-print(student)
+def avto_info(company, model, color, box, make_year, price=None):
+    avto = {
+        'company': company,
+        'model': model,
+        'color': color,
+        'box': box,
+        'make_year': make_year,
+        'price': price
+    }
+    return avto
+
+avto1 = avto_info('bmw', 'x7', 'qora', 'avtomat', 2026)
+avto2 = avto_info('gm', 'gentra', 'oq', 'mexanika', 2023, '200,000,000')
+avtolar = [avto1, avto2]
+print("Onlayn bozorda mavjud bo'lgan mashinalar:")
+for avto in avtolar:
+    if avto['price']:
+        price = avto['price']
+    else:
+        price = "Noma'lum"
+    print(f"{avto['color'].title()} {avto['model'].title()}, Narhi: {price}")
