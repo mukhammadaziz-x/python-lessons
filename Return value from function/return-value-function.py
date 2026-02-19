37c440d363130bf141003de056b783f6f58ae368
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-19 17:10:55 +0500

    Today's theme 'Return value from function'

---
+++ Return value from function/return-value-function.py
@@ -0,0 +1,7 @@
+def toliq_ism_yasa(name, surname):
+    """To'liq ism qaytaruvchi funksiya"""
+    full_name = f"{name} {surname}"
+    print(f"{full_name.title()}")
+
+toliq_ism_yasa('muhammadaziz', 'xabibullayev')
+
