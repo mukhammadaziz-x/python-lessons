7b635fcedeb7523655f5a4580df178cfea8b05e3
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-19 17:23:39 +0500

    Here is function about create full name

--- Return value from function/return-value-function.py
+++ Return value from function/return-value-function.py
@@ -5,17 +5,25 @@
 #
 # toliq_ism_yasa('muhammadaziz', 'xabibullayev')
 
-def toliq_ism_yasa(name, surname):
-    """To'liq ism qaytaruvchi funksiya"""
-    full_name = f"{name.title()} {surname.title()}"
-    return full_name
+# def toliq_ism_yasa(name, surname):
+#     """To'liq ism qaytaruvchi funksiya"""
+#     full_name = f"{name.title()} {surname.title()}"
+#     return full_name
+#
+# student = toliq_ism_yasa('muhammadaziz', 'xabibullayev')
+# teacher = toliq_ism_yasa('sanjar', 'shukurov')
+# doctor = toliq_ism_yasa('umar', 'adkamov')
+#
+# print(f"Darsga kelmagan talabalar: {student.title()}")
+# print(f"Bugun optolmolog {doctor.title()} ishga kelmadi. U bugun dam oladi.")
+# print(f"Programming'dan kiradigan ustozinglar {teacher.title()} bugun betob ekan. Bugun 1 para dars bo'lmaydi.")
 
-student = toliq_ism_yasa('muhammadaziz', 'xabibullayev')
-teacher = toliq_ism_yasa('sanjar', 'shukurov')
-doctor = toliq_ism_yasa('umar', 'adkamov')
+def toliq_ism_yasa(name, surname, middlename=''):
+    if middlename:
+        full_name = f"{name} {surname} {middlename}"
+    else:
+        full_name = f"{name} {surname}"
+    return full_name.title()
 
-print(f"Darsga kelmagan talabalar: {student.title()}")
-print(f"Bugun optolmolog {doctor.title()} ishga kelmadi. U bugun dam oladi.")
-print(f"Programming'dan kiradigan ustozinglar {teacher.title()} bugun betob ekan. Bugun 1 para dars bo'lmaydi.")
-
-
+student = toliq_ism_yasa('ikrom', 'zokirov', "hoshim ogli")
+print(student)
