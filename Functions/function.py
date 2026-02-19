cd4b8318e450880e1f93e09a8822c6c8b22570ef
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-19 14:39:22 +0500

    doc string

--- Functions/function.py
+++ Functions/function.py
@@ -1,5 +1,17 @@
-def salom_ber():
-    """Salom beruvchi funksiya"""
-    print("Assalamu alaykum!")
+# def salom_ber():
+#     """Salom beruvchi funksiya"""
+#     print("Assalamu alaykum!")
+#
+# salom_ber()
 
-salom_ber()
+def salom_ber(name):
+    """Foydalanuvchidan ismini qabul qilib,
+    unga salom beruvchi funksiya"""
+    print(f"Assalamu alaykum, hurmatli {name.title()}!")
+
+salom_ber('muhammadaziz')
+salom_ber('muhammadyusuf')
+
+print(salom_ber.__doc__)
+print(print.__doc__)
+print(max.__doc__)
