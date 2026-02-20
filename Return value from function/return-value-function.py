1823b296b4d7d3649642bec34688d909ea4a9e6e
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-20 17:49:56 +0500

    Foydalanuvchidan uning ma'lumotlarini olib, lug'at ko'rinishida saqlab, konsolga chiroyli qilib chiqarish funksiyasi

--- Return value from function/return-value-function.py
+++ Return value from function/return-value-function.py
@@ -3,10 +3,8 @@
 #     full_name = f"{name} {surname}"
 #     print(f"{full_name.title()}")
 #
-# toliq_ism_yasa('muhammadaziz', 'xabibullayev')
-from pyiceberg.expressions.parser import is_null
-from supabase_auth import model
-
+# toliq_ism_yasa('muhammadaziz', 'xabibullayev')\
+from django.db.models.expressions import result
 
 # def toliq_ism_yasa(name, surname):
 #     """To'liq ism qaytaruvchi funksiya"""
@@ -31,27 +29,27 @@ from supabase_auth import model
 # student = toliq_ism_yasa('ikrom', 'zokirov', "hoshim ogli")
 # print(student)
 
-def avto_info(company, model, color, box, make_year, price=None):
-    avto = {
-        'company': company,
-        'model': model,
-        'color': color,
-        'box': box,
-        'make_year': make_year,
-        'price': price
-    }
-    return avto
-
-avto1 = avto_info('bmw', 'x7', 'qora', 'avtomat', 2026)
-avto2 = avto_info('gm', 'gentra', 'oq', 'mexanika', 2023, '200,000,000')
-avtolar1 = [avto1, avto2]
-print("Onlayn bozorda mavjud bo'lgan mashinalar:")
-for avto in avtolar1:
-    if avto['price']:
-        price = avto['price']
-    else:
-        price = "Noma'lum"
-    print(f"{avto['color'].title()} {avto['model'].title()}, Narhi: {price}")
+# def avto_info(company, model, color, box, make_year, price=None):
+#     avto = {
+#         'company': company,
+#         'model': model,
+#         'color': color,
+#         'box': box,
+#         'make_year': make_year,
+#         'price': price
+#     }
+#     return avto
+#
+# avto1 = avto_info('bmw', 'x7', 'qora', 'avtomat', 2026)
+# avto2 = avto_info('gm', 'gentra', 'oq', 'mexanika', 2023, '200,000,000')
+# avtolar1 = [avto1, avto2]
+# print("Onlayn bozorda mavjud bo'lgan mashinalar:")
+# for avto in avtolar1:
+#     if avto['price']:
+#         price = avto['price']
+#     else:
+#         price = "Noma'lum"
+#     print(f"{avto['color'].title()} {avto['model'].title()}, Narhi: {price}")
 
 # My own range function as rename oraliq()
 # def oraliq(min, max, step=None):
@@ -67,27 +65,75 @@ for avto in avtolar1:
 # for num in oraliq(0, 40, 2):
 #     print(num)
 
-avtolar2 = []
+# avtolar2 = []
+# while True:
+#     print("\nSaytimizdagi avtolar ro'yxtatini shakllantiramiz:")
+#     company = input("Ishlab chiqaruvchi: ")
+#     model = input("Modeli: ")
+#     color = input("Ranggi: ")
+#     box = input("Karobkasi: ")
+#     make_year = input("Ishlab chiqarilgan yili: ")
+#     price = float(input("Narhi: "))
+#
+#     avtolar2.append(avto_info(company, model, color, box, make_year, price))
+#
+#     result = input("Yana qo'shasizmi? (yes/no): ")
+#     if result == 'no':
+#         break
+#
+# print("\nSalonimizdagi avtolar:")
+#
+# for avto in avtolar2:
+#     if avto['price']:
+#         price = avto['price']
+#     else:
+#         price = "No'malum"
+#     print(f"{avto['color'].title()} {avto['model'].title()}, {avto['box']} karobka. Narhi: {price}")
+
+
+
+# HOMEWORK
+# 1. Assignment
+customers = []
+def users(name, surname, age, b_year, b_place, email=None, phone=None):
+    user = {
+        'name': name,
+        'surname': surname,
+        'age': age,
+        'b_year': b_year,
+        'b_place': b_place,
+        'email': email,
+        'phone': phone
+    }
+    return user
+
 while True:
-    print("\nSaytimizdagi avtolar ro'yxtatini shakllantiramiz:")
-    company = input("Ishlab chiqaruvchi: ")
-    model = input("Modeli: ")
-    color = input("Ranggi: ")
-    box = input("Karobkasi: ")
-    make_year = input("Ishlab chiqarilgan yili: ")
-    price = float(input("Narhi: "))
+    name = input("Ismingizni kiriting: ")
+    surname = input("Familiyangizni kiriting: ")
+    age = int(input("Yoshingizni kiriting: "))
+    b_year = int(input("Tug'ilgan yilingizni kiriting: "))
+    b_place = input("Tug'ilgan joyingizni kiriting: ")
 
-    avtolar2.append(avto_info(company, model, color, box, make_year, price))
+    email = input("Email qo'shasizmi? (yes/no): ")
+    if email == 'yes':
+        email = input("Emailingizni kiriting: ")
+    else:
+        print("Rahmat")
+        email = "Noma'lum"
 
-    result = input("Yana qo'shasizmi? (yes/no): ")
+    phone = input("Telefon raqam qo'shasizmi? (yes/no): ")
+    if phone == 'yes':
+        phone = input("Telefon raqamingizni kiriting: ")
+    else:
+        print("Rahmat")
+        phone = "Noma'lum"
+
+    customers.append(users(name, surname, age, b_year, b_place, email, phone))
+    result = input("Yana foydalanuvchi qo'shasizmi? (yes/no): ")
     if result == 'no':
         break
 
-print("\nSalonimizdagi avtolar:")
-
-for avto in avtolar2:
-    if avto['price']:
-        price = avto['price']
-    else:
-        price = "No'malum"
-    print(f"{avto['color'].title()} {avto['model'].title()}, {avto['box']} karobka. Narhi: {price}")
+for user in customers:
+    print(f"{user['name'].title()} {user['surname'].title()}, {user['age']} yoshda, ya'ni {user['b_year']} yilda tug'ilgan. "
+          f"{user['name'].title()} {user['b_place'].capitalize()}da tavallud topgan. "
+          f"Uning emaili: {user['email']}, va uning telefon raqami: {user['phone']}")
