44768128918a0bfaf85722562216114604777e07
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-21 23:48:13 +0500

    Kwargs - avto haqidagi ma'lumotlarni lug'at ko'rinshida qaytaruvchi funksiya

--- Args & Kwargs/args-kwargs.py
+++ Args & Kwargs/args-kwargs.py
@@ -8,14 +8,14 @@
 #
 # print(summa(40, 20, 40))
 
-def avto_info(company, model, **informations):
-    """Avto haqidagi ma'lumotlarni lug'at ko'rinishida qaytaruvchi funksiya"""
-    informations['company'] = company
-    informations['model'] = model
-    return informations
-
-avto1 = avto_info("bmw", "x7", color='blue', make_year=2024)
-avto2 = avto_info("chevrolet", "cobalt", color='white', make_year=2026, box='automatic')
-
-print(avto1)
-print(avto2)
+# def avto_info(company, model, **informations):
+#     """Avto haqidagi ma'lumotlarni lug'at ko'rinishida qaytaruvchi funksiya"""
+#     informations['company'] = company
+#     informations['model'] = model
+#     return informations
+#
+# avto1 = avto_info("bmw", "x7", color='blue', make_year=2024)
+# avto2 = avto_info("chevrolet", "cobalt", color='white', make_year=2026, box='automatic')
+#
+# print(avto1)
+# print(avto2)
