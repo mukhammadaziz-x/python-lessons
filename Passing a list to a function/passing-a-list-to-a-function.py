329cd89e59579bf42fac4e8c05e8bb75ea798a16
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-21 19:42:35 +0500

    Passing a list to a function

---
+++ Passing a list to a function/passing-a-list-to-a-function.py
@@ -0,0 +1,12 @@
+def bahola(names):
+    grades = {}
+    while names:
+        name = names.pop()
+        grade = input(f"{name.title()} ismli talabani baholang: ")
+        grades[name] = int(grade)
+    return grades
+
+students = ['ali', 'vali', 'akmalxon', 'siddiq', 'muhammadaziz']
+grades = bahola(students)
+print(grades)
+print(students)
