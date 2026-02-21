4e86f0e1827a666208c0a1aee0793b2a87772d0f
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-21 23:47:54 +0500

    Kwargs - avto haqidagi ma'lumotlarni lug'at ko'rinshida qaytaruvchi funksiya

--- Args & Kwargs/args-kwargs.py
+++ Args & Kwargs/args-kwargs.py
@@ -1,9 +1,21 @@
 # My own sum function as rename summa()
-def summa(*sonlar):
-    """Kiritilgan sonlarni yig'indisini hisoblaydigan funksiya"""
-    yigindi = 0
-    for son in sonlar:
-        yigindi += son
-    return yigindi
+# def summa(*sonlar):
+#     """Kiritilgan sonlarni yig'indisini hisoblaydigan funksiya"""
+#     yigindi = 0
+#     for son in sonlar:
+#         yigindi += son
+#     return yigindi
+#
+# print(summa(40, 20, 40))
 
-print(summa(40, 20, 40))
+def avto_info(company, model, **informations):
+    """Avto haqidagi ma'lumotlarni lug'at ko'rinishida qaytaruvchi funksiya"""
+    informations['company'] = company
+    informations['model'] = model
+    return informations
+
+avto1 = avto_info("bmw", "x7", color='blue', make_year=2024)
+avto2 = avto_info("chevrolet", "cobalt", color='white', make_year=2026, box='automatic')
+
+print(avto1)
+print(avto2)
