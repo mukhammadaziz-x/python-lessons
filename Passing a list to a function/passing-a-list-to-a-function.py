5396bcef1bea314365169653129f0687e8329c9d
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-21 19:54:28 +0500

    3rd assignment is done

--- Passing a list to a function/passing-a-list-to-a-function.py
+++ Passing a list to a function/passing-a-list-to-a-function.py
@@ -31,3 +31,13 @@
 # katta_harf(names2)
 # print(names2)
 # print(names)
+
+def bahola(names):
+    grades = {}
+    for name in names:
+        grade = input(f"{name.title()} ismli talabani baholang: ")
+        grades[name] = int(grade)
+    return grades
+students = ['ali', 'vali', 'akmalxon', 'siddiq', 'muhammadaziz']
+grades = bahola(students)
+print(grades)
