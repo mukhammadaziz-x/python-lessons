61b376ffe74cea857685ca68c44ca95324cf599d
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-21 19:54:40 +0500

    3rd assignment is done

--- Passing a list to a function/passing-a-list-to-a-function.py
+++ Passing a list to a function/passing-a-list-to-a-function.py
@@ -32,12 +32,12 @@
 # print(names2)
 # print(names)
 
-def bahola(names):
-    grades = {}
-    for name in names:
-        grade = input(f"{name.title()} ismli talabani baholang: ")
-        grades[name] = int(grade)
-    return grades
-students = ['ali', 'vali', 'akmalxon', 'siddiq', 'muhammadaziz']
-grades = bahola(students)
-print(grades)
+# def bahola(names):
+#     grades = {}
+#     for name in names:
+#         grade = input(f"{name.title()} ismli talabani baholang: ")
+#         grades[name] = int(grade)
+#     return grades
+# students = ['ali', 'vali', 'akmalxon', 'siddiq', 'muhammadaziz']
+# grades = bahola(students)
+# print(grades)
