f71db40c1867093c8121a05fafa62f8ed3784c9f
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-22 11:34:17 +0500

    AS kalit so'zi yordamida modul nomini o'zimizga moslab qisqartirishimiz mumkin.

--- Modules/main.py
+++ Modules/main.py
@@ -1,4 +1,7 @@
-import avto_info_mod
+import avto_info_mod as aim
 
-avto1 = avto_info_mod.avto_info('gm', 'malibu', 'oq', 'avtomat', 2020, 24500)
-avto_info_mod.avto_print(avto1)
+# avto1 = avto_info_mod.avto_info('gm', 'malibu', 'oq', 'avtomat', 2020, 24500)
+# avto_info_mod.avto_print(avto1)
+
+avto2 = aim.avto_info('bmw', 'x5', 'kulrang', 'avtomat', 2015, 75000)
+aim.avto_print(avto2)
