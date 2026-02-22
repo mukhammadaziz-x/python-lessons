ea3426213e7c1931c2a8e3cc7a43d10474485836
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-22 15:14:07 +0500

    Map funksiyasi yordamida ro'yxat ichidagi sonlarning ildizini chiqarish

--- The last word/lambda.py
+++ The last word/lambda.py
@@ -1,4 +1,5 @@
-import math
+# import math
+from math import sqrt
 
 # uzunlik = lambda pi, r: 2 * pi * r
 # print(uzunlik(math.pi, 10))
@@ -6,11 +7,14 @@ import math
 # kvadrat = lambda x, y: x ** y
 # print(kvadrat(5, 3))
 
-def daraja(n):
-    return lambda x: x ** n
+# def daraja(n):
+#     return lambda x: x ** n
+#
+# kvadrat = daraja(2) # bu kvadrat chiqarish kodi, daraja(2) bu yerdagi 2 funksiyani n ga berilgan qiymati. Kvadrat o'zgaruvchi nomini lambda uchun berdik, chunki lambda nomsiz funksiya hisoblanadi. Hozir foydalanuvchi argument kiritsa lambda uchun kiritadi. Masalan: n -> daraja(2), x -> lambda x, foydalanuvchi kiritadigan argument.
+# kub = daraja(3) # bu yerda ham tepada yozganimdek ishlaydi, faqat funksiya uchun 3 argument berilgan.
+# print(f"3-ning kvadrati {kvadrat(3)} ga, "
+#       f"kubi {kub(3)} ga teng")
 
-kvadrat = daraja(2) # bu kvadrat chiqarish kodi, daraja(2) bu yerdagi 2 funksiyani n ga berilgan qiymati. Kvadrat o'zgaruvchi nomini lambda uchun berdik, chunki lambda nomsiz funksiya hisoblanadi. Hozir foydalanuvchi argument kiritsa lambda uchun kiritadi. Masalan: n -> daraja(2), x -> lambda x, foydalanuvchi kiritadigan argument.
-kub = daraja(3) # bu yerda ham tepada yozganimdek ishlaydi, faqat funksiya uchun 3 argument berilgan.
-print(f"3-ning kvadrati {kvadrat(3)} ga, "
-      f"kubi {kub(3)} ga teng")
-
+sonlar = list(range(11))
+ildizlar = list(map(sqrt, sonlar))
+print(ildizlar)
