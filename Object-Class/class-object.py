5b54c68cab8e3a02bc5a0cc75805160e467f78f8
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-27 21:22:11 +0500

    Class and Object

--- Object-Class/class-object.py
+++ Object-Class/class-object.py
@@ -17,10 +17,14 @@ class Student:
         self.surname = surname
         self.b_year = b_year
 
+    def tanishtir(self):
+        print(f"Ismim {self.name} familiyam {self.surname}, tug'ilgan yilim {self.b_year}")
+
 student1 = Student('Muslima', 'Faxriddinova', 2000)
 student2 = Student('Xojiakbar', 'Toshtemirov', 2006)
 student3 = Student('Yaxyo', 'Anvarov', 2008)
 
-print(student1.surname)
-print(student3.b_year)
-print(student2.name)
+# print(student1.surname)
+# print(student3.b_year)
+# print(student2.name))
+student2.tanishtir()
