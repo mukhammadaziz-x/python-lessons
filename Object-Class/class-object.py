ebe200aefbf3c85c2215870c0c4454f5d71c4b19
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-27 21:18:01 +0500

    Class and Object

--- Object-Class/class-object.py
+++ Object-Class/class-object.py
@@ -10,3 +10,17 @@
 #
 # salom()
 
+# CLASS
+class Student:
+    def __init__(self, name, surname, b_year):
+        self.name = name
+        self.surname = surname
+        self.b_year = b_year
+
+student1 = Student('Muslima', 'Faxriddinova', 2000)
+student2 = Student('Xojiakbar', 'Toshtemirov', 2006)
+student3 = Student('Yaxyo', 'Anvarov', 2008)
+
+print(student1.surname)
+print(student3.b_year)
+print(student2.name)
