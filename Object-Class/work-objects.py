e3a34d208b40efcf748b4f44d84be8d14c0841b5
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-27 22:12:53 +0500

    Getters and Setters

--- Object-Class/work-objects.py
+++ Object-Class/work-objects.py
@@ -18,11 +18,17 @@ class Student:
     def set_newlevel(self, new_level):
         self.level = new_level
 
-    # def update_level(self):
-    #     self.level += 1
+    def update_level(self):
+        self.level += 1
 
     def get_info(self):
         return f"{self.name} {self.surname}, {self.level}-bosqich talabasi."
+
+    def get_fullname(self):
+        return f"{self.name} {self.surname}"
+
+    def get_age(self, year):
+        return year - self.b_year
 
 user1 = Student('Muhammadaziz', 'Xabibullayev', 2006)
 user1.set_newlevel(2)
