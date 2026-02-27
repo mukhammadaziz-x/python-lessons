210d4476bdf402c472a11f1e5dab16c33c857e27
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-27 21:28:22 +0500

    Class and Object

--- Object-Class/class-object.py
+++ Object-Class/class-object.py
@@ -10,34 +10,34 @@
 #
 # salom()
 
-# CLASS
-class Student:
-    def __init__(self, name, surname, b_year):
-        self.name = name
-        self.surname = surname
-        self.b_year = b_year
-
-    def get_name(self):
-        return self.name
-
-    def get_lastname(self):
-        return self.surname
-
-    def get_age(self, year):
-        return year - self.b_year
-
-    def tanishtir(self):
-        return f"Ismim {self.name} familiyam {self.surname}, tug'ilgan yilim {self.b_year}"
-
-student1 = Student('Muslima', 'Faxriddinova', 2000)
-student2 = Student('Xojiakbar', 'Toshtemirov', 2006)
-student3 = Student('Yaxyo', 'Anvarov', 2008)
+# # CLASS
+# class Student:
+#     def __init__(self, name, surname, b_year):
+#         self.name = name
+#         self.surname = surname
+#         self.b_year = b_year
+#
+#     def get_name(self):
+#         return self.name
+#
+#     def get_lastname(self):
+#         return self.surname
+#
+#     def get_age(self, year):
+#         return year - self.b_year
+#
+#     def tanishtir(self):
+#         return f"Ismim {self.name} familiyam {self.surname}, tug'ilgan yilim {self.b_year}"
+#
+# student1 = Student('Muslima', 'Faxriddinova', 2000)
+# student2 = Student('Xojiakbar', 'Toshtemirov', 2006)
+# student3 = Student('Yaxyo', 'Anvarov', 2008)
 
 # print(student1.surname)
 # print(student3.b_year)
 # print(student2.name))
 
-print(student2.tanishtir())
-print(student2.get_name())
-print(student2.get_lastname())
-print(student2.get_age(2026))
+# print(student2.tanishtir())
+# print(student2.get_name())
+# print(student2.get_lastname())
+# print(student2.get_age(2026))
