3a4690b2bb0e94a8ba3fb8f82ade8832dca97970
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-02-28 22:00:32 +0500

    Homework is successfully done

--- Object-Class/work-objects.py
+++ Object-Class/work-objects.py
@@ -39,6 +39,8 @@
 #         return year - self.b_year
 #
 # user1 = Student('Muhammadaziz', 'Xabibullayev', 2006)
+from List.list import cars
+
 
 # user1.set_newlevel(2)
 # print(user1.get_info())
@@ -89,3 +91,61 @@
 # print(student1.__dict__)
 # print(student1.__dict__.keys())
 # print(see_methods(student1))
+
+# HOMEWORK
+# 1. Assignment
+class Avto:
+    def __init__(self, model, color, box, price, km=0):
+        self.model = model
+        self.color = color
+        self.box = box
+        self.price = price
+        self.km = km
+
+    cars = []
+
+    def get_info(self):
+        return f"{self.color.capitalize()} {self.model}, {self.box} karobka. Narhi: {self.price}"
+
+    def update_km(self, num):
+        if num > 0:
+            self.km += num
+        else:
+            print("Kilometrni kamaytirib bo'lmaydi.")
+
+class Avtosalon:
+    def __init__(self, salon_name, address):
+        self.salon_name = salon_name
+        self.address = address
+        self.cars_in_sale = []
+
+    def add_car(self, car):
+        self.cars_in_sale.append(car)
+
+    def get_cars_info(self):
+        return [car.get_info() for car in self.cars_in_sale]
+
+# Mashina yaratamiz:
+car1 = Avto('BMW', 'black', 'auto', '$150,000')
+car2 = Avto('Mercedes benz', 'white', 'auto', '$350,000')
+
+# Avtosalon yaratamiz:
+my_salon = Avtosalon('General Motors', 'Tashkent, Mirzoulugbek')
+
+# Mashinani salonga qo'shamiz:
+my_salon.add_car(car1)
+my_salon.add_car(car2)
+
+# Kilometrni yangilaymiz:
+car1.update_km(5000)
+
+print(f"Salondagi mashinalar: ({my_salon.salon_name})")
+for info in my_salon.get_cars_info():
+    print(f"- {info}")
+
+print("Methodlarni tekshirish:")
+print(f"Birinchi avto hususiyatlari: {car1.__dict__}")
+
+
+str_methods = [method for method in dir(str) if not method.startswith('__')]
+print(f"str klass metodlari (bir qism): {str_methods[:10]}")
