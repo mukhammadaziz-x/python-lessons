e32d35dcfa83c4db8b08eae129b49380b0e81e99
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-03-03 20:57:56 +0500

    Encapsulation

--- Encapsulation and class methods/encapsulation.py
+++ Encapsulation and class methods/encapsulation.py
@@ -14,3 +14,14 @@ class Avto:
 
     def get_id(self):
         return self.__id
+
+    def add_km(self, km):
+        if km >= 0:
+            self.__km += km
+        else:
+            print("Mashina km kamaytirib bo'lmaydi")
+
+car1 = Avto('GM', 'Malibu', 'black', 30000, 1000)
+
+car1.add_km(150)
+print(car1.get_km())
