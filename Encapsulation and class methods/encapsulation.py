20074531a94aedf7754a7a6c25c2c21b4fc4d210
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-03-03 23:01:57 +0500

    Encapsulation

--- Encapsulation and class methods/encapsulation.py
+++ Encapsulation and class methods/encapsulation.py
@@ -1,9 +1,10 @@
 # import avto
-from avto import Avto, Bus, Train
+# from avto import Avto, Bus, Train
+# from avto import *
 
-car1 = Avto('GM', 'Malibu', 'black', 30000, 1000)
-car2 = Avto('BMW', 'X8', 'white', 353000, 19000)
-car3 = Avto('Toyota', 'KSI 21', 'blue', 11000, 12000)
-
-print(car3.model)
-print(Avto.get_num_avto())
+# car1 = Avto('GM', 'Malibu', 'black', 30000, 1000)
+# car2 = Avto('BMW', 'X8', 'white', 353000, 19000)
+# car3 = Avto('Toyota', 'KSI 21', 'blue', 11000, 12000)
+#
+# print(car3.model)
+# print(Avto.get_num_avto())
