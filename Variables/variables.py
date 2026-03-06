534b0b0c704a2792c3a2eee4b8766269918d3433
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-03-06 23:03:25 +0500

    updated

--- Variables/variables.py
+++ Variables/variables.py
@@ -1,23 +1,23 @@
-name = "Abdulloh"
-age = 4
-address = "Uzbekistan, Andijan region, Andijan city, Objuvoz street 49-home"
-
-print(name)
-print(age)
-print(address)
-
-name = "Muhammadaziz"
-print(name)
-
-word = "Hello World!"
-print(word)
-
-message = "Assalamu alaykum hurmatli talabalar!"
-print(message)
-message = "Bugun sizlar bilan yangi mavzuni ko'rib chiqamiz."
-print(message)
-
-radius = 5
-PI = 3.14159
-round_face = PI * radius ** 2
-print("Radiusi", radius, "ga teng aylananing yuzi =", round_face)
+# name = "Abdulloh"
+# age = 4
+# address = "Uzbekistan, Andijan region, Andijan city, Objuvoz street 49-home"
+#
+# print(name)
+# print(age)
+# print(address)
+#
+# name = "Muhammadaziz"
+# print(name)
+#
+# word = "Hello World!"
+# print(word)
+#
+# message = "Assalamu alaykum hurmatli talabalar!"
+# print(message)
+# message = "Bugun sizlar bilan yangi mavzuni ko'rib chiqamiz."
+# print(message)
+#
+# radius = 5
+# PI = 3.14159
+# round_face = PI * radius ** 2
+# print("Radiusi", radius, "ga teng aylananing yuzi =", round_face)
