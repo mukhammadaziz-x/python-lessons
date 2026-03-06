33dfc8c43fde5cfbf3c772fad18c62a70dfa47c2
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-03-06 22:57:45 +0500

    updated

--- Args & Kwargs/args-kwargs.py
+++ Args & Kwargs/args-kwargs.py
@@ -1,41 +1,41 @@
-# My own sum function as rename summa()
-def summa(*sonlar):
-    """Kiritilgan sonlarni yig'indisini hisoblaydigan funksiya"""
-    yigindi = 0
-    for son in sonlar:
-        yigindi += son
-    return yigindi
-
-print(summa(40, 20, 40))
-
-def avto_info(company, model, **informations):
-    """Avto haqidagi ma'lumotlarni lug'at ko'rinishida qaytaruvchi funksiya"""
-    informations['company'] = company
-    informations['model'] = model
-    return informations
-
-avto1 = avto_info("bmw", "x7", color='blue', make_year=2024)
-avto2 = avto_info("chevrolet", "cobalt", color='white', make_year=2026, box='automatic')
-
-print(avto1)
-print(avto2)
-
-# HOMEWORK
-# 1. Assignment
-def kopaytir(*sonlar):
-    """Istalgancha sonlarni qabul qilib, ularni ko'paytmasini qaytaruvchi funksiya"""
-    kopaytma = 1
-    for son in sonlar:
-        kopaytma *= son
-    return kopaytma
-print(kopaytir(5, 5))
-
-# 2. Assignment
-def student_info(name, surname, **informations):
-    """Talaba haqidagi ma'lumotlarni lug'at ko'rinishida qaytaruvchi funksiya"""
-    informations['name'] = name
-    informations['surname'] = surname
-    return informations
-
-student1 = student_info('muhammadaziz', 'xabibullayev', age=20, b_year=2006, hobby='coding')
-print(student1)
+# # My own sum function as rename summa()
+# def summa(*sonlar):
+#     """Kiritilgan sonlarni yig'indisini hisoblaydigan funksiya"""
+#     yigindi = 0
+#     for son in sonlar:
+#         yigindi += son
+#     return yigindi
+#
+# print(summa(40, 20, 40))
+#
+# def avto_info(company, model, **informations):
+#     """Avto haqidagi ma'lumotlarni lug'at ko'rinishida qaytaruvchi funksiya"""
+#     informations['company'] = company
+#     informations['model'] = model
+#     return informations
+#
+# avto1 = avto_info("bmw", "x7", color='blue', make_year=2024)
+# avto2 = avto_info("chevrolet", "cobalt", color='white', make_year=2026, box='automatic')
+#
+# print(avto1)
+# print(avto2)
+#
+# # HOMEWORK
+# # 1. Assignment
+# def kopaytir(*sonlar):
+#     """Istalgancha sonlarni qabul qilib, ularni ko'paytmasini qaytaruvchi funksiya"""
+#     kopaytma = 1
+#     for son in sonlar:
+#         kopaytma *= son
+#     return kopaytma
+# print(kopaytir(5, 5))
+#
+# # 2. Assignment
+# def student_info(name, surname, **informations):
+#     """Talaba haqidagi ma'lumotlarni lug'at ko'rinishida qaytaruvchi funksiya"""
+#     informations['name'] = name
+#     informations['surname'] = surname
+#     return informations
+#
+# student1 = student_info('muhammadaziz', 'xabibullayev', age=20, b_year=2006, hobby='coding')
+# print(student1)
