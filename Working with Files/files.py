b52ea68d3fd5c6bd4f650b868a53720f9964856e
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-03-06 23:47:52 +0500

    working with files

--- Working with Files/files.py
+++ Working with Files/files.py
@@ -11,21 +11,38 @@
 # print(pi)
 # print(type(pi))
 #
-#
+# READ
 # pi = pi.rstrip()
 # pi = pi.replace('\n', '')
 # pi = float(pi)
 # print(type(pi))
 
-filename = 'data/students.py'
+# filename = 'data/students.py'
 # with open(filename) as file:
 #     for line in file:
 #         print(line)
 
-with open(filename) as file:
-    students = file.readlines()
+# with open(filename) as file:
+#     students = file.readlines()
+#
+# print(students)
 
-print(students)
+# students = [student.rstrip() for student in students]
+# print(students)
 
-students = [student.rstrip() for student in students]
-print(students)
+
+# WRITE
+# filename = 'pi.txt'
+# name = 'Muhammadaziz Xabibullayev'
+# b_year = 2006
+# with open(filename, 'w') as file:
+#     file.write(name + '\n')
+#     file.write(str(b_year) + '\n')
+
+# APPEND
+# filename = 'pi.txt'
+# with open(filename, 'a') as file:
+#     file.write('Muhammadaziz Xabibullayev')
+#     file.write('2006')
+#     file.age('20')
+
