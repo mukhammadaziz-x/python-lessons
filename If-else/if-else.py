7d733345cf20863e6c19678128c7bdf4f98797e4
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-03-06 23:00:04 +0500

    updated

--- If-else/if-else.py
+++ If-else/if-else.py
@@ -1,87 +1,87 @@
-cars = ['audi', 'bmw', 'volvo', 'volkswagen', 'hyundai', 'honda', 'chevrolet']
-for car in cars:
-    print(car.title())
-
-for car in cars:
-    if car == 'bmw':
-        print(car.upper())
-    else:
-        print(car.title())
-
-name = 'ali'
-print(name == 'ali')
-print(name == 'Ali')
-print(name == 'Vali')
-
-print(name.lower() == 'ali')
-
-name = input("What is your name?: ")
-if name.lower() != 'ali':
-    print(f"Sorry, {name.title()}, we're waiting Ali.")
-else:
-    print(f"What's up {name.title()}")
-
-result = float(input("What is 12 multiplied by 6?: "))
-if result != 72:
-    print("The answer is wrong!")
-
-age = int(input("How old are you?: "))
-if age >= 18:
-    print("Welcome to our party!")
-else:
-    print("Cannot enter!")
-
-login = input("Choose a new login: ")
-if len(login) <= 5:
-    print("Login must be more than 5 letters!")
-
-year = int(input("Enter your year of birth: "))
-if 2026 - year < 18:
-    print(f"You're {2026-year} years old.")
-    print("Cannot enter!")
-else:
-    print("Welcome to our party!")
-
-year = int(input("Enter your age: "))
-if year > 65: print("You're in a COVID-19 risk group.")
-
-x, y = 25, 50
-print("x>y") if x > y else print("x<y")
-
-# HOMEWORK
-# 1. Assignment
-cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
-for i in cars:
-    if i == 'gm':
-        print(i.upper())
-    else:
-        print(i.title())
-
-for i in cars:
-    if i != 'gm':
-        print(i.title())
-    else:
-        print(i.upper())
-
-# 2. Assignment
-login = input("Enter your login: ")
-if login.lower() == 'admin':
-    print(f"Welcome Admin. Can you see the user list?")
-else:
-    print(f"Welcome {login.title()}!")
-
-# 3. Assignment
-num1 = input(f"Enter the number 1: ")
-num2 = input(f"Enter the number 2: ")
-
-if num1 == num2:
-    print("The numbers are even!")
-else:
-    print("The numbers aren't equal!")
-
-# 4. Assignment
-number = int(input("Enter the number you want: "))
-if number < 0:
-    print("Negative number")
-else:
-    print("Positive number")
+# cars = ['audi', 'bmw', 'volvo', 'volkswagen', 'hyundai', 'honda', 'chevrolet']
+# for car in cars:
+#     print(car.title())
+#
+# for car in cars:
+#     if car == 'bmw':
+#         print(car.upper())
+#     else:
+#         print(car.title())
+#
+# name = 'ali'
+# print(name == 'ali')
+# print(name == 'Ali')
+# print(name == 'Vali')
+#
+# print(name.lower() == 'ali')
+#
+# name = input("What is your name?: ")
+# if name.lower() != 'ali':
+#     print(f"Sorry, {name.title()}, we're waiting Ali.")
+# else:
+#     print(f"What's up {name.title()}")
+#
+# result = float(input("What is 12 multiplied by 6?: "))
+# if result != 72:
+#     print("The answer is wrong!")
+#
+# age = int(input("How old are you?: "))
+# if age >= 18:
+#     print("Welcome to our party!")
+# else:
+#     print("Cannot enter!")
+#
+# login = input("Choose a new login: ")
+# if len(login) <= 5:
+#     print("Login must be more than 5 letters!")
+#
+# year = int(input("Enter your year of birth: "))
+# if 2026 - year < 18:
+#     print(f"You're {2026-year} years old.")
+#     print("Cannot enter!")
+# else:
+#     print("Welcome to our party!")
+#
+# year = int(input("Enter your age: "))
+# if year > 65: print("You're in a COVID-19 risk group.")
+#
+# x, y = 25, 50
+# print("x>y") if x > y else print("x<y")
+#
+# # HOMEWORK
+# # 1. Assignment
+# cars = ['toyota', 'mazda', 'hyundai', 'gm', 'kia']
+# for i in cars:
+#     if i == 'gm':
+#         print(i.upper())
+#     else:
+#         print(i.title())
+#
+# for i in cars:
+#     if i != 'gm':
+#         print(i.title())
+#     else:
+#         print(i.upper())
+#
+# # 2. Assignment
+# login = input("Enter your login: ")
+# if login.lower() == 'admin':
+#     print(f"Welcome Admin. Can you see the user list?")
+# else:
+#     print(f"Welcome {login.title()}!")
+#
+# # 3. Assignment
+# num1 = input(f"Enter the number 1: ")
+# num2 = input(f"Enter the number 2: ")
+#
+# if num1 == num2:
+#     print("The numbers are even!")
+# else:
+#     print("The numbers aren't equal!")
+#
+# # 4. Assignment
+# number = int(input("Enter the number you want: "))
+# if number < 0:
+#     print("Negative number")
+# else:
+#     print("Positive number")
