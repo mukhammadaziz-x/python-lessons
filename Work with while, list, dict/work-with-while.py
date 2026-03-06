6038b4e6241bc917d7b6f40501ae58ae9b80c7d6
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-03-06 23:05:09 +0500

    updated

--- Work with while, list, dict/work-with-while.py
+++ Work with while, list, dict/work-with-while.py
@@ -1,46 +1,46 @@
-print("Yaqin do'stlaringiz ro'yxatini tuzamiz.")
-friends = []
-n = 1
-while True:
-    question = f"{n}-do'stingizni ismini kiriting: "
-    name = input(question)
-    friends.append(name)
-    repeat = input("Yana ism qo'shishni hohlaysizmi (ha/yo'q): ")
-    n += 1
-    if repeat != 'ha':
-        break
-
-for friend in friends:
-    print(friend.title())
-
-friends = {}
-sign = True
-while sign:
-    name = input("Do'stingizni ismini kiriting: ")
-    age = input(f"{name.title()}ning yoshini kiriting: ")
-    friends[name] = int(age)
-
-    result = input("Yana ma'lumot qo'shasizmi? (ha/yo'q): ")
-    if result != 'ha':
-        break
-
-for name, age in friends.items():
-    print(f"{name.title()} {age} yoshda.")
-
-cars = ['nexia', 'toyota', 'bmw', 'nexia', 'malibu', 'kia k5', 'nexia', 'rolls roys']
-car = 'nexia'
-while car in cars:
-    cars.remove(car)
-
-print(cars)
-
-students = ['akmalxon', 'jamshidbek', 'abdulloh', 'muhammadaziz', 'siddiq']
-graded_students = {}
-while students:
-    student = students.pop()
-    grade = input(f"{student.title()}ning bahosini kiriting: ")
-    print(f"{student.title()} baholandi!")
-    graded_students[student] = int(grade)
-
-for name, grade in graded_students.items():
-    print(f"{name.title()} bu semestrni {grade} bahoga tamomladi.")
+# print("Yaqin do'stlaringiz ro'yxatini tuzamiz.")
+# friends = []
+# n = 1
+# while True:
+#     question = f"{n}-do'stingizni ismini kiriting: "
+#     name = input(question)
+#     friends.append(name)
+#     repeat = input("Yana ism qo'shishni hohlaysizmi (ha/yo'q): ")
+#     n += 1
+#     if repeat != 'ha':
+#         break
+#
+# for friend in friends:
+#     print(friend.title())
+#
+# friends = {}
+# sign = True
+# while sign:
+#     name = input("Do'stingizni ismini kiriting: ")
+#     age = input(f"{name.title()}ning yoshini kiriting: ")
+#     friends[name] = int(age)
+#
+#     result = input("Yana ma'lumot qo'shasizmi? (ha/yo'q): ")
+#     if result != 'ha':
+#         break
+#
+# for name, age in friends.items():
+#     print(f"{name.title()} {age} yoshda.")
+#
+# cars = ['nexia', 'toyota', 'bmw', 'nexia', 'malibu', 'kia k5', 'nexia', 'rolls roys']
+# car = 'nexia'
+# while car in cars:
+#     cars.remove(car)
+#
+# print(cars)
+#
+# students = ['akmalxon', 'jamshidbek', 'abdulloh', 'muhammadaziz', 'siddiq']
+# graded_students = {}
+# while students:
+#     student = students.pop()
+#     grade = input(f"{student.title()}ning bahosini kiriting: ")
+#     print(f"{student.title()} baholandi!")
+#     graded_students[student] = int(grade)
+#
+# for name, grade in graded_students.items():
+#     print(f"{name.title()} bu semestrni {grade} bahoga tamomladi.")
