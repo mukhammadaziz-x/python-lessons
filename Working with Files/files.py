7d37643450dfd87ef994f5eb9a0b4c85a32b11b7
Muhammadaziz <muhammadazizxabibullayev@gmail.com>
2026-03-06 23:15:43 +0500

    working with files

--- Working with Files/files.py
+++ Working with Files/files.py
@@ -1,5 +1,12 @@
 # Working with files
-file = open('pi.txt')
-PI = file.read()
-file.close()
+# file = open('pi.txt')
+# PI = file.read()
+# print(PI)
+# file.close() # Don't recommend
+
+# Correct method
+with open('pi.txt') as file:
+    pi = file.read()
+
+print(pi)
 
