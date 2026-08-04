f8ea130b0a60c6ff0a105fc1b44b42c8287bac87
muhammadaziz-x <muhammadazizxabibullayev@gmail.com>
2026-08-04 12:05:56 +0500

    new code

--- 1-lesson-hello-world/hello-world.py
+++ 1-lesson-hello-world/hello-world.py
@@ -1,15 +1,26 @@
-# print("Hello world!")
-# print("Assalamu alaykum jigar")
-# print(5*8)
-# print(5*10/3)
-# print(8/3*2/1)
+# # My first code in Python
+# print('hello world')
+# print(7+8)
+# print(7*-40)
 #
-# print("Assalamu alaykum")
+# print('Assalomu alaykum')
 # print("Hayrli tong!")
 # print(2+4*2)
-# print(19/5)
+# print(19/2)
 # print(2**4)
-print('hello!')
-
-print('new user detected')
-print('its my new code')
+#
+# # print("Hello world!")
+# # print("Assalamu alaykum jigar")
+# # print(5*8)
+# # print(5*10/3)
+# # print(8/3*2/1)
+# #
+# # print("Assalamu alaykum")
+# # print("Hayrli tong!")
+# # print(2+4*2)
+# # print(19/5)
+# # print(2**4)
+# print('hello!')
+#
+# print('new user detected')
+# print('its my new code')
